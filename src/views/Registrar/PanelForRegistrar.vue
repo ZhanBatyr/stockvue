94b1d04a33d8cf67c7a4d2368a1f61eeb182ac47
abylai <a.zhalgasbai@csi.kz>
2022-06-15 04:52:37 +0600

    IPO кабылдау стрц, Реестр 50%

--- src/views/Registrar/PanelForRegistrar.vue
+++ src/views/Registrar/PanelForRegistrar.vue
@@ -7,31 +7,31 @@
       <div style="display:flex; justify-content: space-around" class="mt-2">
 
         <div class="card card-secondary mt-5" style="width: 500px; height: 300px;">
-          <div class="card-header" v-if="selectedApplication">
-            <h3 class="card-title">{{selectedApplication?.name}}</h3>
+          <div class="card-header" v-if="selectedQuotes">
+            <h3 class="card-title">{{selectedQuotes?.company.name }}</h3>
             <div class="card-tools">
               <!-- Buttons, labels, and many other things can be placed here! -->
               <!-- Here is a label for example -->
-              <span class="badge badge-success">{{selectedApplication?.date}}</span>
+              <span class="badge badge-success">{{selectedQuotes?.createdAt.split('T')[0]}}</span>
             </div>
             <!-- /.card-tools -->
           </div>
           <!-- /.card-header -->
-          <div class="card-body" v-if="selectedApplication">
-            <h4>Ticker: {{ selectedApplication?.ticker }}</h4>
-            <div>Нарықтағы акциялар саны: {{selectedApplication?.outputStocks}}</div>
-            <div>Ішкі акциялар саны: {{selectedApplication?.inputStocks}}</div>
-            <div>Компания секторы: {{selectedApplication?.categoryCompany}}</div>
+          <div class="card-body" v-if="selectedQuotes">
+            <h4>Ticker: {{ selectedQuotes?.symbol }}</h4>
+            <div>Нарықтағы акциялар саны: {{selectedQuotes?.numberOfShares}}</div>
+            <div>Ішкі акциялар саны: {{selectedQuotes?.inTradeNumberOfShares}}</div>
+            <div>Компания секторы: {{selectedSector?.name}}</div>
 
             <div>
-              Коэффициент роста: <span class="badge badge-danger">{{selectedApplication?.coef}} %</span>
+              Коэффициент роста: <span class="badge badge-danger">{{selectedSector?.bonus}} %</span>
             </div>
 
           </div>
-          <div class="card-footer" style="display: flex; justify-content: space-between" v-if="selectedApplication">
+          <div class="card-footer" style="display: flex; justify-content: space-between" v-if="selectedQuotes">
             <button type="button" class="btn btn-secondary btn-sm"
                     data-bs-toggle="modal" data-bs-target="#exampleModal">Сұранысты қайтару</button>
-            <button type="button" class="btn btn-primary btn-sm">Сұранысты нарық ұйымдастырушысына жіберу</button>
+            <button type="button" class="btn btn-primary btn-sm" @click="accept(selectedQuotes.id)">Сұранысты нарық ұйымдастырушысына жіберу</button>
           </div>
         </div>
 
@@ -43,8 +43,8 @@
                 По умолчанию
               </button>
               <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#" @click="newApplications">Жаңа</a></li>
-                <li><a class="dropdown-item" href="#" @click="oldApplications">Ескі</a></li>
+                <li><a class="dropdown-item" href="#" @click="newQuotes">Жаңа</a></li>
+                <li><a class="dropdown-item" href="#" @click="oldQuotes">Ескі</a></li>
                 <li>
                   <hr class="dropdown-divider">
                 </li>
@@ -53,13 +53,13 @@
             </div>
           </div>
 
-          <div class="info-box" v-for="(a, ind) in applications" :key="ind" @click="chooseApplication(a, ind)" style="cursor: pointer">
+          <div class="info-box" v-for="(a, ind) in quotes" :key="a.id" @click="chooseQuotes(a, ind)" style="cursor: pointer">
             <span class="info-box-icon bg-info" style="width: 70px; height: 50px"><i class="far fa-envelope"></i></span>
             <div class="info-box-content">
-              <span class="badge badge-primary">{{ a.date }}</span>
-              <span class="info-box-text">Ticker: {{ a.ticker }}</span>
-              <span class="info-box-text">Эмитент аты: {{ a.name }}</span>
-              <span class="info-box-number">Акциялар саны: {{ a.outputStocks }}</span>
+              <span class="badge badge-primary">{{ a.createdAt.split('T')[0] }}</span>
+              <span class="info-box-text">Ticker: {{ a.symbol }}</span>
+              <span class="info-box-text">Эмитент аты: {{ a.company.name }}</span>
+              <span class="info-box-number">Акциялар саны: {{ a.numberOfShares }}</span>
             </div>
           </div>
         </div>
@@ -79,7 +79,7 @@
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Жоқ</button>
-              <button type="button" class="btn btn-primary" @click="deleteApplication" data-bs-dismiss="modal">Иә</button>
+              <button type="button" class="btn btn-primary" @click="deleteQuotes" data-bs-dismiss="modal">Иә</button>
             </div>
           </div>
         </div>
@@ -89,88 +89,76 @@
 </template>
 
 <script>
+import {PREFIX} from "@/api";
+
 export default {
   name: "PanelForRegistrar",
   data() {
     return {
-      applications: [
-        {
-          date: '2022-04-15',
-          ticker: 'KZTK',
-          name: 'Казактелеком',
-          outputStocks: 400,
-          inputStocks: 4000,
-          categoryCompany: 'Утилиты',
-          coef: 12
-        },
-        {
-          date: '2022-03-20',
-          ticker: 'KSPI',
-          name: 'Каспий банк',
-          outputStocks: 6000,
-          inputStocks: 14000,
-          categoryCompany: 'Информационные технологии',
-          coef: 18
-        },
-        {
-          date: '2022-01-10',
-          ticker: 'TSH',
-          name: 'Тенгиз "Шеврон"',
-          outputStocks: 1800,
-          inputStocks: 4000,
-          categoryCompany: 'Индустрияльные',
-          coef: 9
-        },
-        {
-          date: '2022-04-13',
-          ticker: 'KTZH',
-          name: 'Казакстан темір жолы',
-          outputStocks: 200,
-          inputStocks: 800,
-          categoryCompany: 'Индустрияльные',
-          coef: 5
-        },
-        {
-          date: '2022-02-02',
-          ticker: 'NIT',
-          name: 'НИТ',
-          outputStocks: 750,
-          inputStocks: 3800,
-          categoryCompany: 'Информационные технологии',
-          coef: 18
-        }
-      ],
-      selectedApplication: null,
-      selectedIndex: null
+      selectedQuotes: null,
+      selectedSector: null,
+      selectedIndex: null,
+      quotes: null
     }
   },
-  created() {
-    this.selectedApplication = this.applications[0];
+  async created() {
+    await this.getQuotes();
+    this.selectedQuotes = this.quotes[0];
   },
   methods: {
-    newApplications() {
-      this.applications = this.applications.sort((a, b) =>
-          new Date(b.date) - new Date(a.date));
+    async getQuotes() {
+      await this.axios.get(PREFIX + "/quotes/registrar").then(response => {
+        if (response.status === 200) {
+          this.quotes = response.data.quotes
+        }
+      })
     },
-    oldApplications() {
-      const sortedApplications = this.applications.sort((a, b) =>
-          new Date(b.date) - new Date(a.date));
-      this.applications = sortedApplications.reverse();
+    async accept(quoteId) {
+      await this.axios.post(PREFIX + "/quotes/" + quoteId + "/accept").then(response => {
+        if (response.status === 200) {
+          this.$notify({
+            type: "success",
+            title: "Сәтті қабылданды"
+          })
+          this.$router.go(0)
+        }
+      })
     },
-    defaultApplications() {
+    async getInfoAboutSector() {
+      await this.axios.get(PREFIX + "/sectors/" + this.selectedQuotes?.company.sectorId).then(response => {
+        if (response.status === 200) {
+          this.selectedSector =  response.data
+        }
+      })
+    },
+    newQuotes() {
+      this.quotes = this.quotes.sort((a, b) =>
+          new Date(b.createdAt.split('T')[0]) - new Date(a.createdAt.split('T')[0]));
+    },
+    oldQuotes() {
+      const sortedQuotes = this.quotes.sort((a, b) =>
+          new Date(b.createdAt.split('T')[0]) - new Date(a.createdAt.split('T')[0]));
+      this.quotes = sortedQuotes.reverse();
+    },
+    defaultQuotes() {
       // get request
     },
-    chooseApplication(application, ind) {
-      this.selectedApplication = application;
+    async chooseQuotes(quotes, ind) {
+      this.selectedQuotes = quotes;
       this.selectedIndex = ind;
     },
-    deleteApplication() {
-      this.applications.splice(this.selectedIndex, 1);
+    deleteQuotes() {
+      this.quotes.splice(this.selectedIndex, 1);
 
-      this.selectedApplication = null;
+      this.selectedQuotes = null;
       this.selectedIndex = null;
 
       this.$notify({ type: 'success', title: 'Заявка', text: 'Данная заявка успешно отклонена!' })
+    }
+  },
+  watch: {
+    async 'selectedQuotes'() {
+      await this.getInfoAboutSector();
     }
   }
 }
--- src/views/Registrar/Registries.vue
+++ src/views/Registrar/Registries.vue
@@ -34,7 +34,8 @@
               <div>
                 <label for="sector">Сектор</label>
                 <select class="form-control" name="sector" id="sector" v-model="filterSector" required>
-                  <option v-for="s in dictSectors" :key="s.id" :value="s.bonus">{{s.name}}</option>
+                  <option :value="null">Тандамау</option>
+                  <option v-for="s in dictSectors" :key="s.id" :value="s.id">{{s.name}}</option>
                 </select>
               </div>
 
@@ -52,24 +53,24 @@
            class="mt-2">
 
 
-        <div class="info-box" v-for="(i, ind) in issuers" :key="ind">
+        <div class="info-box" v-for="i in data" :key="i.id">
           <div class="info-box-content">
-            <span class="badge badge-primary" style="width: 100px">{{ i.date }}</span>
-            <h3>Ticker: {{ i.ticker }}</h3>
-            <h2>Название: {{ i.name }}</h2>
-            <span class="info-box-text">Компания секторы: {{ i.categoryCompany }}</span>
-            <span class="info-box-number">Акциялар саны: {{ i.outputStocks }}</span>
+            <span class="badge badge-primary" style="width: 100px">{{ i.createdAt.split('T')[0] }}</span>
+            <h3>Ticker: {{ i.symbol }}</h3>
+            <h2>Название: {{ i.company.name }}</h2>
+            <span class="info-box-text">Компания секторы: {{ i.company.sectorName }}</span>
+            <span class="info-box-number">Акциялар саны: {{ i.numberOfShares}}</span>
           </div>
 
           <div class="small-box bg-gradient-success" style="width: 200px">
             <div class="inner">
-              <h4>{{ i.addUsers }}</h4>
+              <h4>{{ i.company.employees }}</h4>
               <p>Қолданушылар бүгін осы реестрға тіркелген</p>
             </div>
             <div class="icon">
               <i class="fas fa-user-plus" style="width: 50px"></i>
             </div>
-            <a class="small-box-footer" @click="pushRouter(i.ticker)">
+            <a class="small-box-footer" @click="pushRouter(i.symbol)">
               Көбірек ақпарат алу <i class="fas fa-arrow-circle-right"></i>
             </a>
           </div>
@@ -88,92 +89,89 @@ export default {
   data() {
     return {
       dictSectors: [],
-      curData: [
-        {
-          date: '2022-04-15',
-          ticker: 'KZTK',
-          name: 'Казактелеком',
-          outputStocks: 400,
-          inputStocks: 4000,
-          categoryCompany: 'Утилиты',
-          coef: 12,
-          addUsers: 145
-        },
-        {
-          date: '2022-03-20',
-          ticker: 'KSPI',
-          name: 'Каспий банк',
-          outputStocks: 6000,
-          inputStocks: 14000,
-          categoryCompany: 'Информационные технологии',
-          coef: 18,
-          addUsers: 1596
-        },
-        {
-          date: '2022-01-10',
-          ticker: 'TSH',
-          name: 'Тенгиз "Шеврон"',
-          outputStocks: 1800,
-          inputStocks: 4000,
-          categoryCompany: 'Индустрияльные',
-          coef: 9,
-          addUsers: 586
-        },
-        {
-          date: '2022-04-13',
-          ticker: 'KTZH',
-          name: 'Казакстан темір жолы',
-          outputStocks: 200,
-          inputStocks: 800,
-          categoryCompany: 'Индустрияльные',
-          coef: 5,
-          addUsers: 58
-        },
-        {
-          date: '2022-02-02',
-          ticker: 'NIT',
-          name: 'НИТ',
-          outputStocks: 750,
-          inputStocks: 3800,
-          categoryCompany: 'Информационные технологии',
-          coef: 18,
-          addUsers: 105
-        }
-      ],
+      curData: [],
       issuers: [],
+      sectors: [],
+      data: null,
       filtredData: [],
       filterTicker: null,
       filterName: null,
       filterSector: null
     }
   },
-  created() {
-    this.issuers = this.curData;
-    this.get();
+  async created() {
+    // this.issuers = this.curData;
+    await this.get();
+    await this.getData();
+    await this.prepareDate();
   },
   methods: {
+    async getData() {
+      await this.axios.get(PREFIX + "/companies").then(response => {
+        if (response.status === 200) {
+          this.issuers = response.data.companies
+          this.sectors = response.data.sectors
+        }
+      })
+    },
+    async prepareDate() {
+      this.data = [];
+      for (let i = 0; i < this.issuers.length; i++) {
+        await this.axios.get(PREFIX + "/companies/" + this.issuers[i].id)
+            .then(response => {
+          if (response.status === 200) {
+            const curData = response.data.quote
+            curData.company.sectorName = this.sectors[i].name;
+            this.data.push(curData)
+            this.curData.push(curData)
+          }
+        })
+      }
+    },
     pushRouter(ticker) {
       this.$router.push(`/registries/${ticker}`)
     },
     findData() {
-      const filterSectorName = this.dictSectors.find(d => d.value === this.filterSector).name
+      // const filterSectorName = this.data.find(d => d.company.sectorId === this.filterSector).company.name
 
       this.filtredData = [];
-      this.issuers.forEach(i => {
-        if (i.categoryCompany.toLowerCase() === filterSectorName.toLowerCase()) {
-          if (this.filterName !== null && this.filterTicker !== null) {
-            if (i.ticker.toUpperCase() === this.filterTicker.toUpperCase()
-                && i.name.toLowerCase() === this.filterName.toLowerCase()) {
+      this.data = this.curData;
+      this.data.forEach(i => {
+        if (this.filterSector && i.company.sectorId === this.filterSector) {
+          if (this.filterName !== null && this.filterName !== '' && this.filterTicker !== null && this.filterTicker !== '') {
+            if (i.symbol.toUpperCase() === this.filterTicker.toUpperCase()
+                && i.company.name.toLowerCase() === this.filterName.toLowerCase()) {
               this.filtredData.push(JSON.parse(JSON.stringify(i)));
             }
           }
-          else if (this.filterName !== null && this.filterTicker === null) {
-            if (i.name.toLowerCase() === this.filterName.toLowerCase()) {
+          else if (this.filterName !== null && this.filterName !== '' && (this.filterTicker === null || this.filterTicker === '')) {
+            if (i.company.name.toLowerCase() === this.filterName.toLowerCase()) {
               this.filtredData.push(JSON.parse(JSON.stringify(i)));
             }
           }
-          else if (this.filterName === null && this.filterTicker !== null) {
-            if (i.ticker.toUpperCase() === this.filterTicker.toUpperCase()) {
+          else if ((this.filterName === null || this.filterName === '') && this.filterTicker !== null && this.filterTicker !== '') {
+            if (i.symbol.toUpperCase() === this.filterTicker.toUpperCase()) {
+              this.filtredData.push(JSON.parse(JSON.stringify(i)));
+            }
+          }
+          else {
+            this.filtredData.push(JSON.parse(JSON.stringify(i)));
+          }
+        }
+        else if (!this.filterSector) {
+          if (this.filterName !== null && this.filterName !== '' && this.filterTicker !== null && this.filterTicker !== '') {
+            if (i.symbol.toUpperCase() === this.filterTicker.toUpperCase()
+                && i.company.name.toLowerCase() === this.filterName.toLowerCase()) {
+              this.filtredData.push(JSON.parse(JSON.stringify(i)));
+            }
+          }
+          else if (this.filterName !== null && this.filterName !== '' && (this.filterTicker === null || this.filterTicker === '')) {
+            if (i.company.name.toLowerCase() === this.filterName.toLowerCase()) {
+              this.filtredData.push(JSON.parse(JSON.stringify(i)));
+            }
+          }
+          else if ((this.filterName === null || this.filterName === '') && this.filterTicker !== null && this.filterTicker !== '') {
+            if (i.symbol.toUpperCase() === this.filterTicker.toUpperCase()) {
               this.filtredData.push(JSON.parse(JSON.stringify(i)));
             }
           }
@@ -183,20 +181,20 @@ export default {
         }
       });
       if (this.filtredData && this.filtredData.length > 0) {
-        this.issuers = this.filtredData;
+        this.data = this.filtredData;
       }
     },
     resetData() {
       this.filterName = null;
       this.filterTicker = null;
       this.filtredData = [];
-      this.issuers = this.curData;
+      this.data = this.curData;
     },
     async get() {
       await this.axios.get(PREFIX + "/sectors").then(response => {
         if (response.status === 200) {
           this.dictSectors = response.data
-          this.filterSector = this.dictSectors[0].bonus
+          this.filterSector = null;
         }
       })
     }
